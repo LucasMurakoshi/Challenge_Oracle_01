7693b614b5dadef57f959acc84145a8d078aaff4
Lucas <lucasmurakoshi@hotmail.com>
2023-06-21 09:37:55 -0300

    filtrado de mayus, tildes y caracteres especiales

--- js/script.js
+++ js/script.js
@@ -1,27 +1,46 @@
-
+const caracteres = /[áéíóúÁÉÍÓÚ#!¡*-+$"%&/()=?¿']/;
+ 
 const encriptar = ()=>{
     const text = document.getElementById('inputText').value;
+    
 
-    let textEncriptado = text.replace(/e/igm, 'enter');
-    textEncriptado = textEncriptado.replace(/i/igm, 'imes');
-    textEncriptado = textEncriptado.replace(/a/igm, 'ai');
-    textEncriptado = textEncriptado.replace(/o/igm, 'ober');
-    textEncriptado = textEncriptado.replace(/u/igm, 'ufat');
+    if (text !== "") {
+        if (text === text.toLowerCase() && !caracteres.test(text)) {
+            let textEncriptado = text.replace(/e/igm, 'enter');
+            textEncriptado = textEncriptado.replace(/i/igm, 'imes');
+            textEncriptado = textEncriptado.replace(/a/igm, 'ai');
+            textEncriptado = textEncriptado.replace(/o/igm, 'ober');
+            textEncriptado = textEncriptado.replace(/u/igm, 'ufat');
 
-    document.getElementById('container2-arriba').innerHTML = textEncriptado;
+            document.getElementById('container2-arriba').innerHTML = textEncriptado; 
+        } else {
+            alert("No se permiten Mayusculas, Caracteres Especiales ni Tildes");
+        };
+    } else {
+        alert("Ingresar Texto");
+    };
+
     document.getElementById('container2-abajo').innerHTML = '<button class="copiar" id="copybutton" onclick="copiar()">Copiar</button>';
 };
 
 const desencriptar = ()=>{
     const text = document.getElementById('inputText').value;
 
-    let textEncriptado = text.replace(/enter/igm, 'e');
-    textEncriptado = textEncriptado.replace(/imes/igm, 'i');
-    textEncriptado = textEncriptado.replace(/ai/igm, 'a');
-    textEncriptado = textEncriptado.replace(/ober/igm, 'o');
-    textEncriptado = textEncriptado.replace(/ufat/igm, 'u');
-
-    document.getElementById('container2-arriba').innerHTML = textEncriptado;
+    if (text !== "") {
+        if (text === text.toLowerCase() && !tildes.test(text)) {
+            let textEncriptado = text.replace(/enter/igm, 'e');
+            textEncriptado = textEncriptado.replace(/imes/igm, 'i');
+            textEncriptado = textEncriptado.replace(/ai/igm, 'a');
+            textEncriptado = textEncriptado.replace(/ober/igm, 'o');
+            textEncriptado = textEncriptado.replace(/ufat/igm, 'u');
+            document.getElementById('container2-arriba').innerHTML = textEncriptado;
+        } else {
+            alert("No se permiten Mayusculas, Caracteres Especiales ni Tildes");
+        };
+    } else {
+        alert("Ingresar Texto");
+    };
+    
     document.getElementById('container2-abajo').innerHTML = '<button class="copiar" id="copybutton" onclick="copiar()">Copiar</button>';
 }
 
